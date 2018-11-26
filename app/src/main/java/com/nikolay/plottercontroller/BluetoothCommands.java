240d0f6c0475eaff5896d4a94a018bf2b789e0d5
nikolay kanev <lisunger1994@abv.bg>
2018-11-26 21:21:17 +0200

    values

--- app/src/main/java/com/nikolay/plottercontroller/BluetoothCommands.java
+++ app/src/main/java/com/nikolay/plottercontroller/BluetoothCommands.java
@@ -8,10 +8,10 @@ public class BluetoothCommands {
     public static final int COMMAND_DOT =   5;
     public static final int COMMAND_STOP =  6;
 
-    public static final int VALUE_UP =      6; // 1 degree rotation is 5.68 steps
-    public static final int VALUE_DOWN =    6; // 1 degree rotation is 5.68 steps
-    public static final int VALUE_LEFT =    1;
-    public static final int VALUE_RIGHT =   1;
+    public static final int VALUE_UP =      35; // 1 degree rotation is 5.68 steps
+    public static final int VALUE_DOWN =    35; // 1 degree rotation is 5.68 steps
+    public static final int VALUE_LEFT =    3;
+    public static final int VALUE_RIGHT =   3;
 
     public static final int ROTATION_BYJ =  2048;
     public static final int ROTATION_NEMA = 200;
